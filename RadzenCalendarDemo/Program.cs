363a10b3a9f705c9f1ed00afce3f8bbe5b71d9f3
Mark Davis <medavis00@live.com>
2025-03-22 08:32:14 -0700

    init checkin - calendar popup working

--- RadzenCalendarDemo/Program.cs
+++ RadzenCalendarDemo/Program.cs
@@ -1,3 +1,5 @@
+using Radzen;
+using Radzen.Blazor;
 using RadzenCalendarDemo.Components;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -5,6 +7,13 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+//builder.Services.adddb
+
+builder.Services.AddScoped<DialogService>();
+//builder.Services.AddScoped<RadzenNotification>();
+builder.Services.AddScoped<RadzenTooltip>();
+//builder.Services.AddScoped<RadzenContextMenu>();
 
 var app = builder.Build();
 
---
+++ RadzenCalendarDemo/wwwroot/js/Calendar.js
@@ -0,0 +1,15 @@
+﻿// wwwroot/js/calendar.js
+import { Calendar } from '@fullcalendar/core';
+import dayGridPlugin from '@fullcalendar/daygrid';
+
+window.initCalendar = (dotNetRef) => {
+    var calendarEl = document.getElementById('calendar');
+    var calendar = new Calendar(calendarEl, {
+        plugins: [dayGridPlugin],
+        initialView: 'dayGridMonth',
+        dateClick: function (info) {
+            dotNetRef.invokeMethodAsync('OnDateClicked', info.dateStr);
+        }
+    });
+    calendar.render();
+};
