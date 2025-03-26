9c6c843d0db1a1f6b13c03e1d14d85f33d026f41
Mark Davis <medavis00@live.com>
2025-03-25 17:07:30 -0700

    workng popup calendar

---
+++ RadzenCalendarDemo/wwwroot/js/site.js
@@ -0,0 +1,9 @@
+﻿    window.initDateTimePicker = (elementId, dotNetHelper) => {
+        flatpickr(`#${elementId}`, {
+            enableTime: true,
+            dateFormat: "Y-m-d H:i",
+            onChange: function (selectedDates, dateStr, instance) {
+                dotNetHelper.invokeMethodAsync('OnDateSelected', dateStr);
+            }
+        });
+};
